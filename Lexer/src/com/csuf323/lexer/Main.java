82b4d8e9673dc2b9e16aac87de60b13c553d5bc4
Scout2012 <jcbpowell12@gmail.com>
2020-02-09 04:53:11 -0800

    Added beginning steps to Lexer. Doesn't do anything yet!

--- Lexer/src/com/csuf323/lexer/Main.java
+++ Lexer/src/com/csuf323/lexer/Main.java
@@ -1,7 +1,55 @@
 package com.csuf323.lexer;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
 public class Main {
+	
+	public static enum State{
+		START,
+		KEYWORD,
+		SEPARATOR,
+		IDENTIFIER,
+		OPERATOR,
+		REAL,
+		INTEGER
+	}
+	
 	public static void main(String[] args){
-		System.out.println("Initial Commit");
+		String source_path = args[0];
+		BufferedReader fileReader = null;
+		State currentState = State.START;
+		
+		try{
+			fileReader = new BufferedReader(new FileReader(source_path));
+			String text = null;
+			
+			while((text = fileReader.readLine()) != null){
+				for(int i = 0; i < text.length(); i++){
+					step(text.charAt(i), currentState);
+				}
+			}
+			
+		} catch(Exception e){
+			e.printStackTrace();
+		} finally {
+			try {
+				if(fileReader != null){
+					fileReader.close();
+				}
+			} catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void step(Character currentInput, State state){
+		switch(state){
+			case START:
+				break;
+			default:
+				break;
+		}
 	}
 }
