8765b67d9c85656760e7474b89fda745c502ecf0
Scout2012 <jcbpowell12@gmail.com>
2020-02-09 19:26:44 -0800

    Removed unneeded imports

--- Lexer/src/com/csuf323/lexer/Lexer.java
+++ Lexer/src/com/csuf323/lexer/Lexer.java
@@ -1,9 +1,5 @@
 package com.csuf323.lexer;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
