2506942060125f92fce5103cf118d5e2225a4196
Carlos Garcia <carlosgarcia10001@csu.fullerton.edu>
2020-02-15 05:25:37 -0800

    Added and implemented cleanTokenList

--- Lexer/src/com/csuf323/lexer/Lexer.java
+++ Lexer/src/com/csuf323/lexer/Lexer.java
@@ -2,13 +2,7 @@ package com.csuf323.lexer;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Stream;
+import java.util.*;
 
 public class Lexer {
 
@@ -25,12 +19,13 @@ public class Lexer {
 		REAL(3),
 		IN_STRING(4),
 		IN_COMMENT(5),
-		IN_COMPARATOR(6),
-		COMPARATOR(7),
-		SEPARATOR(8),
-		END_STATEMENT(9),
-		DOT_TRANSITION(10);
-
+		SPACE(6),
+		IN_COMPARATOR(7),
+		COMPARATOR(8),
+		SEPARATOR(9),
+		END_STATEMENT(10),
+		DOT_TRANSITION(11),
+		KEYWORD(12);
 		private int id;
 
 		State(int id) {
@@ -43,17 +38,18 @@ public class Lexer {
 	}
 
 	Lexer.State[][] stateTransitionTable = {
-			{ State.START,          State.IDENTIFIER,  State.NUMBER,     State.REAL,         State.IN_STRING,  State.IN_COMMENT, State.IN_COMPARATOR, State.COMPARATOR, State.SEPARATOR, State.END_STATEMENT, State.DOT_TRANSITION},
-			{ State.IDENTIFIER,     State.IDENTIFIER,  State.IDENTIFIER, State.START,        State.START,      State.START,      State.START,         State.START,      State.START,     State.START,         State.START},
-			{ State.NUMBER,         State.START,       State.NUMBER,     State.REAL,         State.START,      State.START,      State.START,         State.START,      State.START,     State.START,         State.REAL},
-			{ State.REAL,           State.START,       State.REAL,       State.REAL,         State.START,      State.START,      State.START,         State.START,      State.START,     State.START,         State.DOT_TRANSITION},
-			{ State.IN_STRING,      State.START,       State.START,      State.START,        State.IN_STRING,  State.START,      State.START,         State.START,      State.START,     State.START,         State.START},
-			{ State.IN_COMMENT,     State.IN_COMMENT,  State.IN_COMMENT, State.IN_COMMENT,   State.IN_COMMENT, State.START,      State.IN_COMMENT,    State.IN_COMMENT, State.IN_COMMENT,State.IN_COMMENT,    State.IN_COMMENT},
-			{ State.IN_COMPARATOR,  State.START,       State.START,      State.START,        State.START,      State.START,      State.START,         State.COMPARATOR, State.SEPARATOR, State.END_STATEMENT, State.DOT_TRANSITION},
-			{ State.COMPARATOR,     State.START,       State.START,      State.START,        State.START,      State.START,      State.START,         State.START,      State.START,     State.START,         State.START},
-			{ State.SEPARATOR,      State.START,       State.START,      State.START,        State.START,      State.START,      State.START,         State.START,      State.START,     State.START,         State.START},
-			{ State.END_STATEMENT,  State.START,       State.START,      State.START,        State.START,      State.START,      State.START,         State.START,      State.START,     State.START,         State.START},
-			{ State.DOT_TRANSITION, State.START,       State.REAL,       State.START,        State.START,      State.START,      State.START,         State.START,      State.START,     State.START,         State.START},
+			{ State.START,          State.IDENTIFIER,  State.NUMBER,     State.REAL,         State.IN_STRING,  State.IN_COMMENT, State.SPACE, State.IN_COMPARATOR, State.COMPARATOR, State.SEPARATOR, State.END_STATEMENT, State.DOT_TRANSITION},
+			{ State.IDENTIFIER,     State.IDENTIFIER,  State.IDENTIFIER, State.START,        State.START,      State.START,      State.START,       State.START,         State.START,      State.START,     State.START,         State.START},
+			{ State.NUMBER,         State.START,       State.NUMBER,     State.REAL,         State.START,      State.START,      State.START,       State.START,         State.START,      State.START,     State.START,         State.REAL},
+			{ State.REAL,           State.START,       State.REAL,       State.REAL,         State.START,      State.START,      State.START,       State.START,         State.START,      State.START,     State.START,         State.DOT_TRANSITION},
+			{ State.IN_STRING,      State.START,       State.START,      State.START,        State.IN_STRING,  State.START,      State.START,       State.START,         State.START,      State.START,     State.START,         State.START},
+			{ State.IN_COMMENT,     State.IN_COMMENT,  State.IN_COMMENT, State.IN_COMMENT,   State.IN_COMMENT, State.IN_COMMENT, State.START,       State.IN_COMMENT,    State.IN_COMMENT, State.IN_COMMENT,State.IN_COMMENT,    State.IN_COMMENT},
+			{ State.SPACE, 	        State.START,       State.START,      State.START,        State.START,      State.START,      State.SPACE,       State.START,         State.START,      State.START,     State.START,         State.START},
+			{ State.IN_COMPARATOR,  State.START,       State.START,      State.START,        State.START,      State.START,      State.START,       State.START,         State.COMPARATOR, State.SEPARATOR, State.END_STATEMENT, State.DOT_TRANSITION},
+			{ State.COMPARATOR,     State.START,       State.START,      State.START,        State.START,      State.START,      State.START,       State.START,         State.START,      State.START,     State.START,         State.START},
+			{ State.SEPARATOR,      State.START,       State.START,      State.START,        State.START,      State.START,      State.START,       State.START,         State.START,      State.START,     State.START,         State.START},
+			{ State.END_STATEMENT,  State.START,       State.START,      State.START,        State.START,      State.START,      State.START,       State.START,         State.START,      State.START,     State.START,         State.START},
+			{ State.DOT_TRANSITION, State.START,       State.REAL,       State.START,        State.START,      State.START,      State.START,       State.START,         State.START,      State.START,     State.START,         State.START},
 	};
 
 	private State prevState = State.START;
@@ -71,8 +67,10 @@ public class Lexer {
 		switch(input){
 			case '0': case '1':case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9':
 				return State.NUMBER;
-			case '(': case ')': case '{': case '}': case ',': case ' ':
+			case '(': case ')': case '{': case '}': case ',':
 				return State.SEPARATOR;
+			case ' ':
+				return State.SPACE;
 			case '>': case '<': case '=':
 				if(this.currState != State.IN_COMPARATOR){
 					return State.IN_COMPARATOR;
@@ -129,29 +127,63 @@ public class Lexer {
 			Token token = new Token();
 			for(int i = 0; i < nextLineRead.length();){
 				char currChar = nextLineRead.charAt(i);
-				if(currChar == '!'){
-					this.isInComment = !this.isInComment;
-				}
-				if(!this.isInComment){
-					this.currState = parseState(this.currState, getColumn(currChar));
-				} else {
+				if(currChar == '!' && this.isInComment){
+					this.isInComment = false;
+					this.prevState = State.IN_COMMENT;
+					this.currState = State.START;
+				} else if(currChar == '!') {
+					this.isInComment = true;
+					this.prevState = this.currState;
 					this.currState = State.IN_COMMENT;
 				}
-				if(this.currState == State.START && this.prevState != State.IN_COMMENT){
-					if(currChar != '\n' && currChar != '\t'){
-						token.tokenName=this.prevState;//System.out.print(this.prevState);
-						token.lexemeName=currToken;//System.out.println("           "  + currToken);
-						tokenList.add(token);
-						token = new Token();
+				if(!this.isInComment && currChar != '!'){
+					this.currState = parseState(this.currState, getColumn(currChar));
+				}
+				if(this.currState == State.START || i == nextLineRead.length()-1){
+					if(this.currState != State.IN_COMMENT) {
+
+						if(this.prevState != State.SPACE){
+							if(i == nextLineRead.length()-1) {
+								State read = parseState(this.currState, getColumn(currChar));
+								if(read == this.prevState) {
+									currToken = currToken + currChar;
+									token.tokenName = this.currState;
+									token.lexemeName = currToken;
+									tokenList.add(token);
+									token = new Token();
+								} else {
+									token.tokenName = this.prevState;
+									token.lexemeName = currToken;
+									tokenList.add(token);
+									token = new Token();
+									currToken = Character.toString(currChar);
+									token.tokenName = read;
+									token.lexemeName = currToken;
+									tokenList.add(token);
+									token = new Token();
+								}
+								++i;
+							} else {
+								token.tokenName = this.prevState;
+								token.lexemeName = currToken;
+								tokenList.add(token);
+								token = new Token();
+							}
+						}
+					} else {
+						++i;
 					}
 					currToken = "";
-				} else {
+				} else if(currChar != ' ' && currChar != '\n' && currChar != '\t'){
 					currToken += currChar;
+					++i;
+				} else {
 					++i;
 				}
 				this.prevState = this.currState;
 			}
 		}
+		tokenList = cleanTokenList(tokenList);
 		return tokenList;
 	}
 
@@ -160,8 +192,28 @@ public class Lexer {
 		List<Token>tokenList = new ArrayList<Token>();
 		tokenList = createTokenList(fileName);
 		for(Token printToken:tokenList){
+			int numSpaces = 15-printToken.tokenName.toString().length();
+			String spaces = "";
+			for(int i= 0; i < numSpaces; i++) {
+				spaces += " ";
+			}
 			System.out.print(printToken.tokenName);
-			System.out.println("           "  + printToken.lexemeName);
+			System.out.println(spaces + printToken.lexemeName);
 		}
 	}
+
+	public List<Token> cleanTokenList(List<Token> list){
+		String keyWordList [] = {"int", "float", "bool", "true", "false", "if", "else", "then", "endif", "while", "whileend", "do", "doend", "for", "forend", "input", "output", "and", "or", "not"};
+		Map<String,State> keyWordMap = new HashMap<String,State>();
+		for(String word:keyWordList){
+			keyWordMap.put(word, State.KEYWORD);
+		}
+		for(Token token:list){
+			if(keyWordMap.containsKey(token.lexemeName)){
+				token.tokenName=keyWordMap.get(token.lexemeName);
+			}
+		}
+		return list;
+	}
+
 }
