c1aa4579600e1bef67cbe063eaa2825b1371b648
arias <arias@Geoffe>
2020-02-16 02:23:58 -0800

    Added text file output

--- Lexer/src/com/csuf323/lexer/Lexer.java
+++ Lexer/src/com/csuf323/lexer/Lexer.java
@@ -1,7 +1,10 @@
 package com.csuf323.lexer;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -194,13 +197,36 @@ public class Lexer {
 		List<Token>tokenList;
 		tokenList = createTokenList(fileName);
 		for(Token printToken:tokenList){
-			int numSpaces = 15-printToken.tokenName.toString().length();
-			String spaces = "";
-			for(int i= 0; i < numSpaces; i++) {
-				spaces += " ";
+			if(printToken.tokenName != State.IN_COMMENT) {
+				int numSpaces = 15-printToken.tokenName.toString().length();
+				String spaces = "";
+				for(int i= 0; i < numSpaces; i++) {
+					spaces += " ";
+				}
+				String textToAppend = printToken.tokenName + spaces + printToken.lexemeName;
+				
+				BufferedWriter writer = null;
+				try {
+					writer = new BufferedWriter(new FileWriter("output_lexeme.txt", true));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}  
+				try {
+					writer.newLine();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				try {
+					writer.write(textToAppend);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				try {
+					writer.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
-			System.out.print(printToken.tokenName);
-			System.out.println(spaces + printToken.lexemeName);
 		}
 	}
 
@@ -210,6 +236,7 @@ public class Lexer {
 		for(String word:keyWordList){
 			keyWordMap.put(word, State.KEYWORD);
 		}
+		
 		for(Token token:list){
 			if(keyWordMap.containsKey(token.lexemeName)){
 				token.tokenName=keyWordMap.get(token.lexemeName);
